f602163cbc612fb00ad7662f19888665ff6c20bb
hisanori-ito <86972518+hisanori-ito@users.noreply.github.com>
2022-03-11 13:09:07 +0000

    add comment routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
+
   root to: "homes#top"
   get "/about"=>"homes#about"
-  
+
   devise_for :users
-  
-  resources :posts
-  
+
+  resources :posts do
+    resources :comments, only: [:create, :destroy]
+  end
+
 end
