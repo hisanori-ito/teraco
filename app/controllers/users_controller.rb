c1b4c9ffb2644a61fdc061025b9b26cd1bbcaf49
hisanori-ito <86972518+hisanori-ito@users.noreply.github.com>
2022-03-11 18:54:31 +0000

    update users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,32 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
   end
 
   def show
+    @user = User.find(params[:id])
+    @posts = @user.posts
   end
 
   def edit
+    @user = User.find(params[:id])
   end
+  
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    redirect_to user_path(user)
+  end
+  
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :introduction)
+  end
+  
 end
