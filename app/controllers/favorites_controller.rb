9363c690c5b7d6154b43deb0dccdea01dffc8e83
hisanori-ito <86972518+hisanori-ito@users.noreply.github.com>
2022-03-25 19:46:12 +0000

    fix authority

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,7 +1,5 @@
 class FavoritesController < ApplicationController
   
-  before_action :authenticate_user!
-  
   def index
     @favorites = Favorite.where(user_id: params[:user_id]).page(params[:page]).per(16)
     @user = User.find(params[:user_id])
--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,6 +1,4 @@
 class NotificationsController < ApplicationController
-  
-  before_action :authenticate_user!
    
   def index
     @notifications = current_user.to_me_notifications.page(params[:page]).per(10)
