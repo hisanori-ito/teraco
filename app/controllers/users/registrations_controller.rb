107966f6487045aef1e61d8fdccf6d49f7aefa78
hisanori-ito <86972518+hisanori-ito@users.noreply.github.com>
2022-03-30 03:20:32 +0000

    fix mail function

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -6,7 +6,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
     #superでdeviseのcreateアクションを呼ぶ
     super
     #NotificationMailerクラスのsend_registrationメソッドを呼び、POSTから受け取ったuserのemailとnameを渡す
-    NotificationMailer.send_registration(params[:user][:name], params[:user][:email]).deliver
+    if @user.save
+      NotificationMailer.send_registration(params[:user][:name], params[:user][:email]).deliver
+    end
   end
   
   # ↓ゲストユーザーは退会できない
